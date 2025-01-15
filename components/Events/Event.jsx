6e7bf01409d2ebd6331291f7ad7624dab9b80e8c
Jorge Matos <jisapjisap@gmail.com>
2025-01-15 20:58:52 +0100

    Add event image and type display to Event component

--- components/Events/Event.jsx
+++ components/Events/Event.jsx
@@ -6,6 +6,18 @@ import { BiCalendar, BiMap, BiTime } from "react-icons/bi"
 const Event = ({ event }) => {
   return (
     <div className="bg-white/5 hover:bg-white/10 transition-all h-[440px] rounded-3xl flex flex-col justify-start p-4 w-[320px] sm:w-full mx-auto sm:mx-0">
+      <div className="relative w-full h-[320px] mb-10">
+        <Image 
+          src={event.img_sm}
+          alt="event image"
+          fill
+          quality={100}
+          className="rounded-2xl object-cover"
+        />
+        <div className="absolute -bottom-[24px] left-4 bg-accent w-[110px] h-[48px] text-[13px] uppercase font-medium rounded-full flex items-center justify-center">
+          {event.type}
+        </div>
+      </div>
       {event.title}
     </div>
   )

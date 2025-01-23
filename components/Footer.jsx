825b2eeeffa6ab155be01a23899f9660ea33ff46
Jorge Matos <jisapjisap@gmail.com>
2025-01-23 19:07:34 +0100

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -30,24 +30,40 @@ const Footer = () => {
     <footer className="bg-accent bg-pattern bg-cover bg-blend-multiply pt-16">
       <div className="container mx-auto border-b border-white/40">
         <div className="flex flex-col max-w-[550px] mx-auto text-center">
-         <div className="mb-9">
-          <h2 className="h2 mb-3">Your Event Connection</h2>
-          <p>Join our list for exclusice event updates and insider tips.</p>
-         </div>
+          <div className="mb-9">
+            <h2 className="h2 mb-3">Your Event Connection</h2>
+            <p>Join our list for exclusice event updates and insider tips.</p>
+          </div>
 
-         <form 
-          className="relative flex items-center mb-16"
-         >
-          <input 
-            type="text" 
-             placeholder="Enter your email"
-             className="pl-8 w-full h-[60px] rounded-full outline-none placeholder:text-primary/80 text-primary text-sm"
+          <form
+            className="relative flex items-center mb-16"
+          >
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="pl-8 w-full h-[60px] rounded-full outline-none placeholder:text-primary/80 text-primary text-sm"
             />
-         </form>
+            <button className="bg-secondary hover:bg-secondary-hover transition-all w-[114px] h-[53px] rounded-full text-sm uppercase absolute right-1">
+              Join
+            </button>
+          </form>
 
-         <div className="mb-[72px]">socials</div>  
+          <div className="mb-[72px] flex gap-8 mx-auto">
+            {socials.map((icon, index) => {
+              return(
+                <Link href={icon.path} key={index} className="relative w-[20px] h-[20px]">
+                  <Image 
+                    src={icon.src}
+                    alt="social icon"
+                    fill
+                  />
+                </Link>
+              )
+            })}
+          </div>
         </div>
       </div>
+
       <div className="py-8">
         <div className="container mx-auto">
           <div className="flex flex-col md:flex-row gap-6 items-center justify-between">
@@ -55,7 +71,7 @@ const Footer = () => {
               href="/"
               className="relative flex w-[78px] h-[30px]"
             >
-              <Image 
+              <Image
                 src="/assets/footer/logo.svg"
                 fill
                 alt="Logo"

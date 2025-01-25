a0821972ef761bb27f893324d742cf95ce85c566
Jorge Matos <jisapjisap@gmail.com>
2025-01-25 19:13:54 +0100

    Fix event propagation in CustomSelect component to prevent unintended menu closure

--- components/CustomSelect.jsx
+++ components/CustomSelect.jsx
@@ -21,7 +21,10 @@ const CustomSelect = ({ event }) => {
 
   return (
     <div 
-      onClick={() => setShowMenu((prev) => !prev)}
+      onClick={(e) => {
+        setShowMenu((prev) => !prev)
+        e.stopPropagation()  
+      }}
       className='custom-select bg-secondary w-full h-[64px] rounded-full flex items-center justify-between px-8 relative cursor-pointer select-none'
     >
       <div className="flex items-center gap-2 w-full">
@@ -42,8 +45,12 @@ const CustomSelect = ({ event }) => {
           {event.seats.map((seat, index) => (
             <li 
               key={index} 
-              onClick={() => handleSeat(seat.seat, seat.price)}
+              onClick={(e) => {
+                handleSeat(seat.seat, seat.price)
+                e.stopPropagation()
+              }}
               className="cursor-pointer hover:bg-white/5 px-8 py-5"
+
             >
               <div className="flex justify-between">
                 <div className="capitalize">{seat.seat}</div>
